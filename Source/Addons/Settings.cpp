42fb756d77456635a94d0caf8b44e53d8263af68
BillMe2024 <169838318+BillMe2024@users.noreply.github.com>
2024-05-30 08:11:48 +0200

    Update Settings.cpp

--- Source/Addons/Settings.cpp
+++ Source/Addons/Settings.cpp
@@ -2,8 +2,29 @@
 
 void Settings::Init()
 {
-	ShowModMenuWindowInput = GetPrivateProfileInt("Keybinds", "model", ShowModMenuWindowInput, "./scripts/gf2asi.ini");
-	ShowImGuiDemoWindowInput = GetPrivateProfileInt("Keybinds", "demo", ShowImGuiDemoWindowInput, "./scripts/gf2asi.ini");
-	FlyModeUpInput = GetPrivateProfileInt("Keybinds", "flyup", FlyModeUpInput, "./scripts/gf2asi.ini");
-	FlyModeDownInput = GetPrivateProfileInt("Keybinds", "flydown", FlyModeDownInput, "./scripts/gf2asi.ini");
+	LPTSTR path = (LPTSTR) "./scripts/gf2asi.ini";
+	ShowModMenuWindowInput = GetPrivateProfileInt("Keybinds", "model", ShowModMenuWindowInput, path);
+	ShowImGuiDemoWindowInput = GetPrivateProfileInt("Keybinds", "demo", ShowImGuiDemoWindowInput, path);
+	FlyModeUpInput = GetPrivateProfileInt("Keybinds", "flyup", FlyModeUpInput, path);
+	FlyModeDownInput = GetPrivateProfileInt("Keybinds", "flydown", FlyModeDownInput, path);
+}
+
+int Settings::GetShowModMenuWindowInput()
+{
+	return ShowModMenuWindowInput;
+}
+
+int Settings::GetShowImGuiDemoWindowInput()
+{
+	return ShowImGuiDemoWindowInput;
+}
+
+int Settings::GetFlyModeUpInput()
+{
+	return FlyModeUpInput;
+}
+
+int Settings::GetFlyModeDownInput()
+{
+	return FlyModeDownInput;
 }
