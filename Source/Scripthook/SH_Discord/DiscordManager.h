8e208157c94566821f8043d6a749355e0ba3f11b
BillMe2024 <169838318+BillMe2024@users.noreply.github.com>
2024-06-21 19:44:37 +0200

    Update DiscordManager.h

--- Source/Scripthook/SH_Discord/DiscordManager.h
+++ Source/Scripthook/SH_Discord/DiscordManager.h
@@ -1,23 +1,33 @@
 #pragma once
 
+// RenderWare Framework
+#include "SDK/EARS_Framework/Game_Framework/Core/EventHandler/CEventHandler.h"
+
 // CPP
 #include <ctime>
 
 /**
  * Discord Manager for the Scripthook
  */
-class DiscordManager
+class DiscordManager : public RWS::CEventHandler
 {
 public:
+
+	DiscordManager();
+
+	//~ Begin RWS::CEventHandler Interface
+	virtual void HandleEvents(const RWS::CMsg& MsgEvent) override;
+	//~ End RWS::CEventHandler Interface
 
 	/**
 	 * Called when the manager needs to be initialised
 	 */
 	void Open();
 
-	void OnTick();
-
 private:
+
+	// Called when iMsgRunningTick event is detected
+	void OnTick();
 
 	uint32_t uCurrentCityID = 0;
 };
