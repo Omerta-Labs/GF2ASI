521f854a7e270facb33cd8653bc1cd8453e4c780
Greavesy1899 <connor.greaves1999@gmail.com>
2024-09-20 12:26:40 +0100

    RwMaths: Expand on RwV3d

---
+++ Source/SDK/EARS_Common/RwMaths.cpp
@@ -0,0 +1,89 @@
+#include "RwMaths.h"
+
+RwV3d::RwV3d()
+	: m_X(0.0f)
+	, m_Y(0.0f)
+	, m_Z(0.0f)
+{
+
+}
+
+RwV3d::RwV3d(const RwV3d& Other)
+	: m_X(Other.m_X)
+	, m_Y(Other.m_Y)
+	, m_Z(Other.m_Z)
+{
+
+}
+
+RwV3d::RwV3d(const float InValue)
+	: m_X(InValue)
+	, m_Y(InValue)
+	, m_Z(InValue)
+{
+
+}
+
+RwV3d RwV3d::operator+(const RwV3d& InA) const
+{
+	RwV3d Out;
+	RwV3dAdd(Out, *this, InA);
+	return Out;
+}
+
+RwV3d RwV3d::operator-(const RwV3d& InA) const
+{
+	RwV3d Out;
+	RwV3dSub(Out, *this, InA);
+	return Out;
+}
+
+RwV3d RwV3d::operator*(const RwV3d& InA) const
+{
+	RwV3d Out;
+	RwV3dScale(Out, *this, InA);
+	return Out;
+}
+
+RwV3d RwV3d::operator*(const float InA) const
+{
+
+}
+
+bool RwV3d::operator!=(const RwV3d& InA) const
+{
+	return m_X != InA.m_X || m_Y != InA.m_Y || m_Z != InA.m_Z;
+}
+
+bool RwV3d::operator==(const RwV3d& InA) const
+{
+	return m_X == InA.m_X && m_Y == InA.m_Y && m_Z == InA.m_Z;
+}
+
+void RwV3dAdd(RwV3d& Out, const RwV3d& InA, const RwV3d& InB)
+{
+	Out.m_X = InA.m_X + InB.m_X;
+	Out.m_Y = InA.m_Y + InB.m_Y;
+	Out.m_Z = InA.m_Z + InB.m_Z;
+}
+
+void RwV3dAssign(RwV3d& Out, const RwV3d& InA)
+{
+	Out.m_X = InA.m_X;
+	Out.m_Y = InA.m_Y;
+	Out.m_Z = InA.m_Z;
+}
+
+void RwV3dSub(RwV3d& Out, const RwV3d& InA, const RwV3d& InB)
+{
+	Out.m_X = InA.m_X - InB.m_X;
+	Out.m_Y = InA.m_Y - InB.m_Y;
+	Out.m_Z = InA.m_Z - InB.m_Z;
+}
+
+void RwV3dScale(RwV3d& Out, const RwV3d& InA, const RwV3d& InB)
+{
+	Out.m_X = InA.m_X * InB.m_X;
+	Out.m_Y = InA.m_Y * InB.m_Y;
+	Out.m_Z = InA.m_Z * InB.m_Z;
+}
--- Source/SDK/EARS_Common/RwMaths.h
+++ Source/SDK/EARS_Common/RwMaths.h
@@ -1,7 +1,39 @@
 #pragma once
 
+/**
+ * Customised version of Renderware's RwV3d
+ * This is built more to make use of new C++ tech
+ * Renderware is a little dated now.
+ */
 struct RwV3d
 {
+public:
+
+	// default constructor. sets all to 0.0f
+	RwV3d();
+	
+	// sets all to specified 'InValue'
+	RwV3d(const float InValue);
+
+	// copies the values from the other RwV3d
+	RwV3d(const RwV3d& Other);
+
+	// operator overloads
+
+	// addition
+	RwV3d operator+(const RwV3d& InA) const;
+
+	// subtract
+	RwV3d operator-(const RwV3d& InA) const;
+
+	// multiply
+	RwV3d operator*(const RwV3d& InA) const;
+	RwV3d operator*(const float InA) const;
+
+	// equality checks
+	bool operator==(const RwV3d& InA) const;
+	bool operator!=(const RwV3d& InA) const;
+
 	float m_X = 0.0f;
 	float m_Y = 0.0f;
 	float m_Z = 0.0f;
@@ -18,3 +50,10 @@ struct RwMatrixTag
 	RwV3d m_Pos;
 	float pad3;
 };
+
+// functions
+// (from engine, feel free to use operator overloads for simplicity)
+void RwV3dAdd(RwV3d& Out, const RwV3d& InA, const RwV3d& InB);
+void RwV3dAssign(RwV3d& Out, const RwV3d& InA);
+void RwV3dSub(RwV3d& Out, const RwV3d& InA, const RwV3d& InB);
+void RwV3dScale(RwV3d& Out, const RwV3d& InA, const RwV3d& InB);
