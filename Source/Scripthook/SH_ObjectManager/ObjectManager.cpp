d1d016a86197315e6df4ed76270d5acd837da6ff
gfbillbardi <169838318+gfbillbardi@users.noreply.github.com>
2024-09-20 14:40:15 +0200

    Update ObjectManager.cpp

--- Source/Scripthook/SH_ObjectManager/ObjectManager.cpp
+++ Source/Scripthook/SH_ObjectManager/ObjectManager.cpp
@@ -7,7 +7,6 @@
 #include "SDK/EARS_Framework/Core/AttributeHandler/CAttributeHandler.h"
 #include "SDK/EARS_Framework/Core/SimManager/SimManager.h"
 #include "SDK/EARS_Framework/Core/Entity/Entity.h"
-#include "SDK/EARS_Godfather/Modules/Player/Player.h"
 
 void Mod::ObjectManager::Spawn(const RWS::CAttributePacket& AttrPacket, const RwV3d Position)
 {
